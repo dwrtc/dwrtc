93f0bb6d19a7406d7ae97e30fe4e1062076ba06f
Lukas Röllin <lsroellin@gmail.com>
2018-09-25 14:37:26 +0200

    Revert "Add TomP2P as dependency"
    This reverts commit 146bc5289bbedb3e0dcdfb5afc0bd06948853baa.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.2.51"
     id("org.sonarqube") version "2.6"
-    id("java")
 }
 
 group = "ch.hsr.dsl.dwms"
@@ -11,13 +10,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven(url = "http://tomp2p.net/dev/mvn/")
 }
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    compile(group = "net.tomp2p", name = "tomp2pall", version = "5.0-Beta8")
-
 }
 
 tasks.withType<KotlinCompile> {
