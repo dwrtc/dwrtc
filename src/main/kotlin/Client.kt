19533c3058eab29541c7621a867f65f0177514e2
Demian <demian@nine.ch>
2018-10-03 14:37:56 +0200

    fix typo

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -5,6 +5,6 @@ import kotlin.properties.ObservableProperty
 abstract class Client {
     internal abstract fun sendMessage(message: Message)
 
-    abstract val recieveMessage: ObservableProperty<Message>
+    abstract val receiveMessage: ObservableProperty<Message>
     abstract fun createMessage(messageBody: String): Message
 }
