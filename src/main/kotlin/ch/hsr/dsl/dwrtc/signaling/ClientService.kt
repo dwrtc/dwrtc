9c1754ae5981619d88a62f2ea060bf733a2e37f4
Demian <thde@users.noreply.github.com>
2018-10-04 11:15:17 +0200

    add logging fixes #14 (#15)

--- src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/ClientService.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/ClientService.kt
@@ -1,5 +1,6 @@
 package ch.hsr.dsl.dwrtc.signaling
 
+import mu.KLogging
 import net.tomp2p.dht.PeerBuilderDHT
 import net.tomp2p.p2p.PeerBuilder
 import net.tomp2p.peers.Number160
@@ -7,26 +8,42 @@ import net.tomp2p.peers.PeerAddress
 import java.util.*
 
 class ClientService() {
+    companion object : KLogging()
+
     private val peerId = UUID.randomUUID().toString()
     private val peer = PeerBuilderDHT(PeerBuilder(Number160.createHash(peerId)).ports(4000).start()).start()!!
 
+    init {
+        logger.info { "creating service" }
+    }
+
     constructor(bootstrapPeerAddress: PeerConnectionDetails) : this() {
+        logger.info { "using bootstrap peer $bootstrapPeerAddress" }
+
         peer.peer().bootstrap().inetAddress(bootstrapPeerAddress.ipAddress).ports(bootstrapPeerAddress.port).start()
                 .awaitListeners()
     }
 
     fun addClient(sessionId: String): InternalClient {
+        logger.info { "add client $sessionId" }
+
         peer.put(Number160.createHash(sessionId)).`object`(peer.peerAddress()).start().awaitUninterruptibly()
         return InternalClient(peer, sessionId)
     }
 
     fun removeClient(internalClient: InternalClient) {
+        logger.info { "remove client ${internalClient.sessionId}" }
+
         peer.remove(Number160.createHash(internalClient.sessionId)).all().start().awaitUninterruptibly()
     }
 
     fun findClient(sessionId: String): ExternalClient {
+        logger.info { "try to find client $sessionId" }
+
         val peerIdGet = peer.get(Number160.createHash(sessionId)).start().awaitUninterruptibly()
         return if (peerIdGet.isSuccess) {
+            logger.info { "found client" }
+
             val peerAddress = peerIdGet.data().`object`() as PeerAddress
             ExternalClient(sessionId, peerAddress)
         } else throw Exception("No peer found under session ID $sessionId")
--- src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/InternalClient.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/InternalClient.kt
@@ -1,9 +1,14 @@
 package ch.hsr.dsl.dwrtc.signaling
 
+import mu.KLogging
 import net.tomp2p.dht.PeerDHT
 
 class InternalClient(private val peer: PeerDHT, val sessionId: String) {
+    companion object : KLogging()
+
     fun sendMessage(messageBody: String, recipient: ExternalClient) {
+        logger.info { "send message $messageBody to $recipient" }
+
         peer.peer()
             .sendDirect(recipient.peerAddress)
             .`object`(MessageDto(sessionId, recipient.sessionId, messageBody))
@@ -11,6 +16,8 @@ class InternalClient(private val peer: PeerDHT, val sessionId: String) {
     }
 
     fun onReceiveMessage(emitter: (ExternalClient, MessageDto) -> Any) {
+        logger.info { "register emitter for message receiving" }
+
         peer.peer().objectDataReply { senderPeerAddress, messageDto ->
             if (messageDto is MessageDto && messageDto.senderSessionId == sessionId) emitter(
                 ExternalClient(
