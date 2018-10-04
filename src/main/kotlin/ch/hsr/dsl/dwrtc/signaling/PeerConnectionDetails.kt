141229d8a3aec5a01df9519cfb6bceb126d9d066
Lukas Röllin <lsroellin@gmail.com>
2018-10-04 08:53:14 +0200

    fix snakecase variable naming

--- src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/PeerConnectionDetails.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/PeerConnectionDetails.kt
@@ -2,6 +2,6 @@ package ch.hsr.dsl.dwrtc.signaling
 
 import java.net.InetAddress
 
-class PeerConnectionDetails(ip_address_string: String, val port: Int) {
-    val ip_address: InetAddress = InetAddress.getByName(ip_address_string)
+class PeerConnectionDetails(ipAddressString: String, val port: Int) {
+    val ipAddress: InetAddress = InetAddress.getByName(ipAddressString)
 }
