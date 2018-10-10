ff65d22244ceec355597b220768564063f9a7ef7
Lukas Röllin <lsroellin@gmail.com>
2018-10-10 16:07:11 +0200

    add static file serving

--- src/main/kotlin/ch/hsr/dsl/dwrtc/websocket/Main.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/websocket/Main.kt
@@ -4,10 +4,13 @@ import ch.hsr.dsl.dwrtc.signaling.ClientService
 import ch.hsr.dsl.dwrtc.util.WEBSERVER_PORT
 import ch.hsr.dsl.dwrtc.util.config
 import io.javalin.Javalin
+import io.javalin.staticfiles.Location
 
 fun main(args: Array<String>) {
-    val app = Javalin.create().start(config[WEBSERVER_PORT])
-    app.get("/") { ctx -> ctx.result("root") }
+    val app = Javalin.create()
+            .enableStaticFiles("src/main/resources/public/", Location.EXTERNAL)
+            // or .enableStaticFiles("/public") to use Class Path
+            .start(config[WEBSERVER_PORT])
 
     val service = ClientService()
 
