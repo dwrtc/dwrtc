dcdb04e59379653c51be538fcb0ed389bf0a4b79
Lukas Röllin <lsroellin@gmail.com>
2018-10-10 12:40:33 +0200

    support command line arguments (-Dhttp.port=)

--- src/main/kotlin/ch/hsr/dsl/dwrtc/util/Config.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/util/Config.kt
@@ -3,7 +3,6 @@ package ch.hsr.dsl.dwrtc.util
 import com.natpryce.konfig.*
 
 val WEBSOCKET_PORT = Key("http.port", intType)
-val config = EnvironmentVariables overriding
+val config = ConfigurationProperties.systemProperties() overriding
+        EnvironmentVariables overriding
         ConfigurationProperties.fromResource("defaults.properties")
-// TODO find out how command line parameters are supposed to be used
-
