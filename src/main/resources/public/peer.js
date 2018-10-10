b2e6a3936119df3aff3bd514c883ff2a777a7c5e
Lukas Röllin <lsroellin@gmail.com>
2018-10-10 17:31:33 +0200

    use callbacks for message dispatching

--- src/main/resources/public/peer.js
+++ src/main/resources/public/peer.js
@@ -8,34 +8,70 @@ class SignalingMessage {
 }
 
 let id
+let socket
+
 window.onload = () => {
   const websocketUrl = "ws://localhost:7000/ws"
   let simplePeer = window.SimplePeer
-  let socket = new WebSocket(websocketUrl)
-  socket.onopen = _ => setupSocket(socket)
+  socket = new WebSocket(websocketUrl)
+  socket.onopen = _ => setupSocket()
 }
 
-const setupSocket = socket => {
-  socket.onmessage = event => onMessage(event)
+const setupSocket = () => {
+  socket.onmessage = event =>
+    onMessage(
+      event,
+      onWebsocketIdMessage,
+      onWebsocketErrorMessage,
+      onSignalingMessage
+    )
   socket.onclose = event =>
     console.log(`OnClose (Reason ${event.reason}, Code ${event.code})`)
   socket.onerror = event => console.log(`OnError ${event}`)
-
-  let message = new SignalingMessage(
-    "d3ceaa27-d260-4fcb-aa3a-8039e43c44d1",
-    "Hallo"
-  )
-  socket.send(JSON.stringify(message))
+  setupPeer()
 }
 
-const onMessage = event => {
+const onMessage = (
+  event,
+  onWebsocketIdMessage,
+  onWebsocketErrorMessage,
+  onSignalingMessage
+) => {
   let message = JSON.parse(event.data)
   switch (message.type) {
     case "WebsocketIdMessage":
-      id = message.id
+      onWebsocketIdMessage(message)
       break
     case "WebsocketErrorMessage":
-      console.error(message.error)
+      onWebsocketErrorMessage(message)
+      break
+    case "SignalingMessage":
+      onSignalingMessage(message)
       break
   }
+}
+
+const onWebsocketIdMessage = message => {
+  id = message.id
+  console.log(`ID: ${id}`)
+}
+
+const onWebsocketErrorMessage = message => {
+  console.error(message.error)
+}
+
+const onSignalingMessage = message => {
+  console.log(
+    `Message. Sender: ${message.senderSessionId}, Recipient: ${
+      message.recipientSessionId
+    }, Message: ${message.messageBody}`
+  )
+}
+
+const setupPeer = () => {
+  let message = new SignalingMessage(
+    "d9c3c68f-64ed-4abb-9009-3f0f044adfda",
+    "Blablabla"
+  )
+  socket.send(JSON.stringify(message))
 }
