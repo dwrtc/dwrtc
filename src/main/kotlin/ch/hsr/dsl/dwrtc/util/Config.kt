45e3b9c460f16ad7e0d29637d7fe2883587380e1
Lukas Röllin <lsroellin@gmail.com>
2018-10-10 12:45:30 +0200

    use better constant name

--- src/main/kotlin/ch/hsr/dsl/dwrtc/util/Config.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/util/Config.kt
@@ -2,7 +2,7 @@ package ch.hsr.dsl.dwrtc.util
 
 import com.natpryce.konfig.*
 
-val WEBSOCKET_PORT = Key("http.port", intType)
+val WEBSERVER_PORT = Key("http.port", intType)
 val config = ConfigurationProperties.systemProperties() overriding
         EnvironmentVariables overriding
         ConfigurationProperties.fromResource("defaults.properties")
