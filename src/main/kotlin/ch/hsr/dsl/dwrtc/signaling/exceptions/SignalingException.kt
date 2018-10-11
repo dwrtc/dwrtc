07fa326fa47e457b630320b2c1d89c05e2100c3e
Lukas Röllin <lsroellin@gmail.com>
2018-10-11 09:37:54 +0200

    remove override var

--- src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/exceptions/SignalingException.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/exceptions/SignalingException.kt
@@ -1,5 +1,5 @@
 package ch.hsr.dsl.dwrtc.signaling.exceptions
 
-open class SignalingException(override var message: String) : Exception(message)
+open class SignalingException(message: String) : Exception(message)
 
-class ClientNotFoundException(override var message: String) : SignalingException(message)
+class ClientNotFoundException(message: String) : SignalingException(message)
