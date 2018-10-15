7795968442e1dfda2f17539900938527973e7e94
Demian <demian@nine.ch>
2018-10-15 19:04:26 +0200

    use arrow syntax

--- src/main/resources/public/debug/debug.js
+++ src/main/resources/public/debug/debug.js
@@ -9,15 +9,12 @@ const connectClicked = event => {
   event.preventDefault()
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: true })
-    .then(function(stream) {
+      .then((stream) => {
       console.debug("Got user media")
       peer = new SimplePeer({ initiator: false, stream: stream })
       peer.on("signal", data => {
         // Peer wants to send signalling data
         console.debug(`Send Signal message: ${data}`)
       })
-    })
-    .catch(function(err) {
-      console.error(`Could not get user media or other error in setup, ${err}`)
-    })
+      }).catch((err) => console.error(`Could not get user media or other error in setup, ${err}`))
 }
