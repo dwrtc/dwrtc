58680c2345e76c25915955e0ce9805686b3aa9dc
Lukas Röllin <lsroellin@gmail.com>
2018-10-24 13:34:44 +0200

    reformat

--- src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/Message.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/signaling/Message.kt
@@ -6,9 +6,9 @@ open class Message(val type: String) : Serializable
 
 data class SignalingMessage(var senderSessionId: String?, var recipientSessionId: String?, val messageBody: String) :
         Message("SignalingMessage") {
-        init {
-                this.senderSessionId = senderSessionId?.trim()
-                this.recipientSessionId = recipientSessionId?.trim()
-        }
+    init {
+        this.senderSessionId = senderSessionId?.trim()
+        this.recipientSessionId = recipientSessionId?.trim()
+    }
 }
 
