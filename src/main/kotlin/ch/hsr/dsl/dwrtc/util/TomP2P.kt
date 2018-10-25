314d2e2db36eea790851da351b5650717bc30ab9
Lukas Röllin <lsroellin@gmail.com>
2018-10-25 09:00:09 +0200

    doc

--- src/main/kotlin/ch/hsr/dsl/dwrtc/util/TomP2P.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/util/TomP2P.kt
@@ -5,6 +5,9 @@ import net.tomp2p.dht.FutureGet
 import net.tomp2p.futures.BaseFuture
 import net.tomp2p.futures.BaseFutureAdapter
 
+/**
+ * Raised whenever an operation has been completed
+ */
 fun BaseFuture.onComplete(emitter: (future: BaseFuture) -> Unit) {
     this.addListener(object : BaseFutureAdapter<BaseFuture>() {
         override fun operationComplete(future: BaseFuture) {
@@ -13,10 +16,21 @@ fun BaseFuture.onComplete(emitter: (future: BaseFuture) -> Unit) {
     })
 }
 
+/**
+ * Raised whenever an operation is successful
+ *
+ * TomP2P defines success, such that *the DHT has responded*. Therefore, *empty responses are considered a success*
+ */
 fun BaseFuture.onSuccess(emitter: () -> Unit) {
     this.onComplete { result -> if (result.isSuccess) emitter() }
 }
 
+/**
+ * Raised whenever an operation has failed
+ *
+ * TomP2P defines failure, such that *the DHT has **not** responded*.
+ * Therefore, this only fires in cases where the DHT connection
+ */
 fun BaseFuture.onFailure(emitter: (failedReason: String) -> Unit) {
     this.onComplete { result -> if (result.isFailed) emitter(result.failedReason()) }
 }
