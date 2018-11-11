045735e4e79c21778ac3fd2bb597607c692bf720
Lukas Röllin <lsroellin@gmail.com>
2018-11-11 14:49:40 +0100

    add http.ip config variable

--- src/main/kotlin/ch/hsr/dsl/dwrtc/websocket/Main.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/websocket/Main.kt
@@ -3,13 +3,22 @@ package ch.hsr.dsl.dwrtc.websocket
 import ch.hsr.dsl.dwrtc.signaling.ClientService
 import ch.hsr.dsl.dwrtc.util.*
 import io.javalin.Javalin
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
 
 /** Main start point. Fires up the Webserver. */
 fun main(args: Array<String>) {
     val app = Javalin.create()
             // .enableStaticFiles("src/main/resources/public/", Location.EXTERNAL)
-            .enableStaticFiles("/public")
-            .start(config[WEBSERVER_PORT])
+            .enableStaticFiles("/public").server {
+                Server().apply {
+                    connectors = arrayOf(ServerConnector(this).apply {
+                        this.host = config[WEBSERVER_IP]
+                        this.port = config[WEBSERVER_PORT]
+                    })
+                }
+            }
+            .start()
 
     val clientService =
             ClientService(config.getOrNull(BOOTSTRAP_IP), config.getOrNull(BOOTSTRAP_PORT), config.getOrNull(PEER_PORT))
