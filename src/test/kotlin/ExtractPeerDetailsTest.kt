2de22b3bbe57dba53e60a798fe98c7d9dba07549
Lukas Röllin <lsroellin@gmail.com>
2018-11-15 13:55:13 +0100

    add tests

---
+++ src/test/kotlin/ExtractPeerDetailsTest.kt
@@ -0,0 +1,35 @@
+package test
+
+import ch.hsr.dsl.dwrtc.signaling.PeerConnectionDetails
+import ch.hsr.dsl.dwrtc.util.config.extractPeerDetails
+import io.kotlintest.matchers.collections.shouldBeEmpty
+import io.kotlintest.matchers.collections.shouldContainExactly
+import io.kotlintest.specs.WordSpec
+
+class ExtractionTest : WordSpec() {
+
+    init {
+        "an empty string list" should {
+            val extractPeerDetails = extractPeerDetails(emptyList())
+            "yield an empty peer list" {
+                extractPeerDetails.shouldBeEmpty()
+            }
+        }
+        "a string list with one elements" should {
+            val extractPeerDetails = extractPeerDetails(listOf("127.0.0.1:5000"))
+            "yield exactly one result" {
+                val expected = listOf(PeerConnectionDetails("127.0.0.1", 5000))
+                extractPeerDetails.shouldContainExactly(expected)
+            }
+        }
+        "a string list with multiple elements" should {
+            val extractPeerDetails = extractPeerDetails(listOf("127.0.0.1:5000", "127.0.0.1:4000"))
+            "yield all results result" {
+                val expected = listOf(
+                        PeerConnectionDetails("127.0.0.1", 5000),
+                        PeerConnectionDetails("127.0.0.1", 4000))
+                extractPeerDetails.shouldContainExactly(expected)
+            }
+        }
+    }
+}
