f60c7ff0ae2cff87dda0898d82812d87858e12ce
Lukas Röllin <lsroellin@gmail.com>
2018-11-20 12:49:01 +0100

    use constant for sub path

--- src/main/kotlin/ch/hsr/dsl/dwrtc/websocket/Main.kt
+++ src/main/kotlin/ch/hsr/dsl/dwrtc/websocket/Main.kt
@@ -8,16 +8,18 @@ import mu.KotlinLogging
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.ServerConnector
 
+const val WEB_SUB_PATH = "/web/public/"
+
 /** Main start point. Fires up the Webserver. */
 fun main(args: Array<String>) {
     val logger = KotlinLogging.logger {}
     val app = Javalin.create()
     if (config.getOrElse(PACKED_RESOURCES, true)) {
         logger.info { "using packed web resources" }
-        app.enableStaticFiles("/public")
+        app.enableStaticFiles(WEB_SUB_PATH)
     } else {
         logger.info { "using file system web resources" }
-        app.enableStaticFiles("src/main/resources/public/", Location.EXTERNAL)
+        app.enableStaticFiles("src/main/resources$WEB_SUB_PATH", Location.EXTERNAL)
     }
     app.server {
         Server().apply {
